c5b59e1da2850bacd1773dbd5026aca9f3ffa7e8
Emma Jaeger <emmajaeger1@gmail.com>
2018-10-17 15:37:23 -0400

    zdbjksab

--- grid/grid.js
+++ grid/grid.js
@@ -2,7 +2,6 @@ for (var i = 0; i < 10; i = i + 1) {
     for (var j = 0; j < 10; j = j + 1) {
         console.log("Row " + i + ", Column " + j);
 
-        // The next line is the only JS you'll have to change.
         if (i < 5) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
@@ -15,14 +14,13 @@ for (var i = 0; i < 10; i = i + 1) {
     document.write('<br />');
 }
 
-
 document.write('<br />');
+
 
 for (var i = 0; i < 10; i = i + 1) {
     for (var j = 0; j < 10; j = j + 1) {
         console.log("Row " + i + ", Column " + j);
 
-        // The next line is the only JS you'll have to change.
         if (j < 5) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
@@ -37,12 +35,53 @@ for (var i = 0; i < 10; i = i + 1) {
 
 document.write('<br />');
 
+
 for (var i = 0; i < 10; i = i + 1) {
     for (var j = 0; j < 10; j = j + 1) {
         console.log("Row " + i + ", Column " + j);
 
-        // The next line is the only JS you'll have to change.
-        if (i % 1) {
+        if (j - i == 0) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (i - j > -1) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        if (i % 2) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
         } else {
