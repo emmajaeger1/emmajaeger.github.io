b6b7599fd14995f4a70feebb3d4944f64981bf0d
Emma Jaeger <emmajaeger1@gmail.com>
2018-12-13 18:47:09 -0500

    asdfhjksj

--- homepage/index.js
+++ homepage/index.js
@@ -1,3 +1,9 @@
+{
+window.addEventListener("mousemove", onMouseMove)
+
+function onMouseMove(evt) {
+    //console.log(evt)
+}
 var box = document.querySelector("#box")
 
 window.addEventListener("mousemove",onMouseMove)
