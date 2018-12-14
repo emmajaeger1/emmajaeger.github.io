b2183f4aeb6642fa64b0dced357de5fba4d8e208
Emma Jaeger <emmajaeger1@gmail.com>
2018-12-13 21:14:21 -0500

    aiwejf

--- homepage/index.js
+++ homepage/index.js
@@ -7,8 +7,9 @@ function onMouseMove(evt) {
 var box = document.querySelector("#box")
 
 window.addEventListener("mousemove",onMouseMove)
-function onMouseMove(evt){
+function onMouseMove(evt)
+{
     console.log(evt.x + " " + evt.y)
     box.style.left = evt.x + "px"
     box.style.top = evt.y + "px"
-  
+}
